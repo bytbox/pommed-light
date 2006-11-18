a748c7e619d077a8620ca65988a46e915fbe192c
Julien BLACHE <jb@jblache.org>
2006-11-18 22:07:15 +0000

    Move evdev functions to evdev.c

---
+++ src/evdev.h
@@ -0,0 +1,16 @@
+#ifndef __EVDEV_H__
+#define __EVDEV_H__
+
+void
+process_evdev_events(int fd);
+
+int
+evdev_is_geyser4(unsigned short vendor, unsigned short product);
+
+int
+open_evdev(struct pollfd **fds);
+
+void
+close_evdev(struct pollfd **fds, int nfds);
+
+#endif /* !__EVDEV_H__ */
--- src/mbpeventd.c
+++ src/mbpeventd.c
@@ -16,99 +16,20 @@
 
 #include <errno.h>
 
-#include <linux/input.h>
-
 #include <smbios/SystemInfo.h>
 
 #include "mbpeventd.h"
 #include "kbd_backlight.h"
 #include "lcd_backlight.h"
 #include "cd_eject.h"
+#include "evdev.h"
+
 
 /* Machine type */
 int machine;
 
 /* Used by signal handlers */
 static int running;
-
-
-void
-process_evdev_events(int fd)
-{
-  int ret;
-
-  struct input_event ev;
-
-  ret = read(fd, &ev, sizeof(struct input_event));
-
-  if (ret != sizeof(struct input_event))
-    return;
-
-  if (ev.type == EV_KEY)
-    {
-      /* key released - we don't care */
-      if (ev.value == 0)
-	return;
-
-      switch (ev.code)
-	{
-	  case K_LCD_BCK_DOWN:
-	    debug("\nKEY: LCD backlight down\n");
-
-	    lcd_backlight_step(STEP_DOWN);
-	    break;
-
-	  case K_LCD_BCK_UP:
-	    debug("\nKEY: LCD backlight up\n");
-
-	    lcd_backlight_step(STEP_UP);
-	    break;
-
-	  case K_AUDIO_MUTE:
-	    debug("\nKEY: audio mute\n");
-	    break;
-
-	  case K_AUDIO_DOWN:
-	    debug("\nKEY: audio down\n");
-	    break;
-
-	  case K_AUDIO_UP:
-	    debug("\nKEY: audio up\n");
-	    break;
-
-	  case K_VIDEO_TOGGLE:
-	    debug("\nKEY: video toggle\n");
-	    break;
-
-	  case K_KBD_BCK_OFF:
-	    debug("\nKEY: keyboard backlight off\n");
-
-	    kbd_backlight_set(KBD_BACKLIGHT_OFF);
-	    break;
-
-	  case K_KBD_BCK_DOWN:
-	    debug("\nKEY: keyboard backlight down\n");
-
-	    kbd_backlight_step(STEP_DOWN);
-	    break;
-
-	  case K_KBD_BCK_UP:
-	    debug("\nKEY: keyboard backlight up\n");
-
-	    kbd_backlight_step(STEP_UP);
-	    break;
-
-	  case K_CD_EJECT:
-	    debug("\nKEY: CD eject\n");
-
-	    cd_eject();
-	    break;
-
-	  default:
-	    break;
-	}
-    }
-}
 
 
 int
@@ -149,130 +70,6 @@ check_machine_smbios(void)
   SMBIOSFreeMemory(prop);
 
   return ret;
-}
-
-
-int
-evdev_is_geyser4(unsigned short vendor, unsigned short product)
-{
-  if (vendor != USB_VENDOR_ID_APPLE)
-    return 0;
-
-  return (product == USB_PRODUCT_ID_GEYSER4_ISO);
-}
-
-int
-open_evdev(struct pollfd **fds)
-{
-  int ret;
-  int i, j;
-
-  int found = 0;
-  int fd[32];
-
-  unsigned short id[4];
-  unsigned long bit[EV_MAX][NBITS(KEY_MAX)];
-  char evdev[32];
-
-  for (i = 0; i < EVDEV_MAX; i++)
-    {
-      ret = snprintf(evdev, 32, "%s%d", EVDEV_BASE, i);
-
-      if ((ret <= 0) || (ret > 31))
-	return -1;
-
-      fd[i] = open(evdev, O_RDWR);
-      if (fd[i] < 0)
-	{
-	  if (errno != ENOENT)
-	    debug("Could not open %s: %s\n", evdev, strerror(errno));
-
-	  continue;
-	}
-
-      ioctl(fd[i], EVIOCGID, id);
-
-      if (!evdev_is_geyser4(id[ID_VENDOR], id[ID_PRODUCT]))
-	{
-	  debug("Discarding evdev %d vid 0x%04x, pid 0x%04x\n", i, id[ID_VENDOR], id[ID_PRODUCT]);
-
-	  close(fd[i]);
-	  fd[i] = -1;
-
-	  continue;
-	}
-
-      memset(bit, 0, sizeof(bit));
-
-      ioctl(fd[i], EVIOCGBIT(0, EV_MAX), bit[0]);
-
-      if (!test_bit(1, bit[0]))
-	{
-	  debug("Discarding evdev %d with no key event type (not a keyboard)\n", i);
-
-	  close(fd[i]);
-	  fd[i] = -1;
-
-	  continue;
-	}
-      else if (test_bit(2, bit[0]))
-	{
-	  debug("Discarding evdev %d with event type >= 2 (not a keyboard)\n", i);
-
-	  close(fd[i]);
-	  fd[i] = -1;
-
-	  continue;
-	}
-
-      found++;
-    }
-
-  debug("Found %d devices\n", found);
-
-  *fds = (struct pollfd *) malloc(found * sizeof(struct pollfd));
-
-  if (*fds == NULL)
-    {
-      for (i = 0; i < EVDEV_MAX; i++)
-	{
-	  if (fd[i] > 0)
-	    close(fd[i]);
-	}
-
-      debug("Out of memory for %d pollfd structs\n", found);
-
-      return -1;
-    }
-
-  j = 0;
-  for (i = 0; i < EVDEV_MAX && j < found; i++)
-    {
-      if (fd[i] < 0)
-	continue;
-
-      (*fds)[j].fd = fd[i];
-      (*fds)[j].events = POLLIN;
-      j++;
-    }
-
-  return found;
-}
-
-void
-close_evdev(struct pollfd **fds, int nfds)
-{
-  int i;
-
-  if (*fds != NULL)
-    {
-      for (i = 0; i < nfds; i++)
-	close((*fds)[i].fd);
-
-      free(*fds);
-    }
-
-  *fds = NULL;
 }
 
 
