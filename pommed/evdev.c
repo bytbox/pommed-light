3da5dcce69415021df22c32660f45aa720587e6f
Julien BLACHE <jb@jblache.org>
2007-12-06 20:29:09 +0000

    Handle several inotify events at once.
    Also handle inotify events with len > sizeof(struct inotify_event), otherwise we end up busy-looping.

--- pommed/evdev.c
+++ pommed/evdev.c
@@ -294,52 +294,83 @@ evdev_inotify_process(void)
 {
   int ret;
   int fd;
+  int qsize;
 
-  struct inotify_event ie[2];
+  struct inotify_event *all_ie;
+  struct inotify_event *ie;
   char evdev[32];
 
-  ret = read(ifd, ie, sizeof(ie));
+  /* Determine the size of the inotify queue */
+  ret = ioctl(ifd, FIONREAD, &qsize);
   if (ret < 0)
     {
-      logdebug("inotify read failed: %s\n", strerror(errno));
+      logdebug("Could not determine inotify queue size: %s\n", strerror(errno));
 
       return;
     }
 
-  /* ie[0] contains the inotify event information
-   * the memory space for ie[1] contains the name of the file
-   * see the inotify documentation
-   */
-
-  if ((ie[0].len == 0) || (ie[0].name == NULL))
+  all_ie = (struct inotify_event *) malloc(qsize);
+  if (all_ie == NULL)
     {
-      logdebug("inotify event with no name\n");
+      logmsg(LOG_ERR, "Could not allocate %d bytes for inotify events");
 
       return;
     }
 
-  logdebug("Found new event device %s/%s\n", EVDEV_DIR, ie[0].name);
-
-  if (strncmp("event", ie[0].name, 5))
+  ret = read(ifd, all_ie, qsize);
+  if (ret < 0)
     {
-      logdebug("Discarding %s/%s\n", EVDEV_DIR, ie[0].name);
+      logmsg(LOG_WARNING, "inotify read failed: %s", strerror(errno));
 
+      free(all_ie);
       return;
     }
 
-  ret = snprintf(evdev, 32, "%s/%s", EVDEV_DIR, ie[0].name);
+  /* ioctl(FIONREAD) returns the number of bytes, now we need the number of elements */
+  qsize /= sizeof(struct inotify_event);
 
-  if ((ret <= 0) || (ret > 31))
-    return;
-
-  fd = open(evdev, O_RDWR);
-  if (fd < 0)
+  /* Loop through all the events we got */
+  for (ie = all_ie; (ie - all_ie) < qsize; ie += (1 + (ie->len / sizeof(struct inotify_event))))
     {
-      if (errno != ENOENT)
-	logmsg(LOG_WARNING, "Could not open %s: %s", evdev, strerror(errno));
+      /* ie[0] contains the inotify event information
+       * the memory space for ie[1+] contains the name of the file
+       * see the inotify documentation
+       */
+
+      if ((ie->len == 0) || (ie->name == NULL))
+	{
+	  logdebug("inotify event with no name\n");
+
+	  continue;
+	}
+
+      logdebug("Found new event device %s/%s\n", EVDEV_DIR, ie->name);
+
+      if (strncmp("event", ie->name, 5))
+	{
+	  logdebug("Discarding %s/%s\n", EVDEV_DIR, ie->name);
+
+	  continue;
+	}
+
+      ret = snprintf(evdev, sizeof(evdev), "%s/%s", EVDEV_DIR, ie->name);
+
+      if ((ret <= 0) || (ret >= sizeof(evdev)))
+	continue;
+
+      fd = open(evdev, O_RDWR);
+      if (fd < 0)
+	{
+	  if (errno != ENOENT)
+	    logmsg(LOG_WARNING, "Could not open %s: %s", evdev, strerror(errno));
+
+	  continue;
+	}
+
+      evdev_try_add(fd);
     }
 
-  evdev_try_add(fd);
+  free(all_ie);
 }
 
 
