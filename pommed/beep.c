04527656cd983fd315abca7f06b49542a1c823b3
Julien BLACHE <jb@jblache.org>
2007-12-12 11:23:22 +0000

    Rework & fix the audio sample loading code.

--- pommed/beep.c
+++ pommed/beep.c
@@ -272,7 +272,7 @@ beep_load_sample(char *filename)
 {
   AFfilehandle affd;     /* filehandle for soundfile from libaudiofile */
   AFframecount framecount;
-  int dummy, channels, byteorder, framesize, precision, err = 0;
+  int dummy, channels, byteorder, framesize, precision;
   struct sample *sample;
 
   int ret;
@@ -282,64 +282,67 @@ beep_load_sample(char *filename)
     return NULL;
 
   affd = afOpenFile(filename, "r", 0);
-  if (affd > 0)
-    {
-      afGetSampleFormat(affd, AF_DEFAULT_TRACK, &dummy, &precision);
-      channels = afGetChannels(affd, AF_DEFAULT_TRACK);
-      byteorder = afGetVirtualByteOrder(affd, AF_DEFAULT_TRACK);
-      framesize = (int) afGetFrameSize(affd, AF_DEFAULT_TRACK, 0);
-      framecount = afGetFrameCount(affd, AF_DEFAULT_TRACK);
-      sample->speed = (int) afGetRate(affd, AF_DEFAULT_TRACK);
-
-      if (channels <= 2)
-	sample->channels = channels;
-      else
-	err = -1;
-
-      if (precision == 8)
-	sample->format = SND_PCM_FORMAT_S8;
-      else if (precision == 16)
-	{
-	  if (byteorder == AF_BYTEORDER_LITTLEENDIAN)
-	    sample->format = SND_PCM_FORMAT_S16_LE;
-	  else
-	    sample->format = SND_PCM_FORMAT_S16_BE;
-	}
-      else
-	err = -1;
-
-      if (err == 0)
-	{
-	  sample->framesize = framesize;
-	  sample->framecount = framecount;
-	  sample->audiodatalen = framecount * framesize;
-
-	  sample->audiodata = (char *) malloc(sample->audiodatalen);
-	  if (sample->audiodata != NULL)
-	    {
-	      ret = afReadFrames(affd, AF_DEFAULT_TRACK, sample->audiodata, framecount);
-	      if (ret != framecount)
-		{
-		  free(sample->audiodata);
-		  err = -1;
-		}
-	    }
-	  else
-	    ret = -1;
-	}
-      afCloseFile(affd);
-    }
-
-  if (err == -1)
+  if (!affd)
     {
       free(sample);
       return NULL;
     }
 
+  afGetSampleFormat(affd, AF_DEFAULT_TRACK, &dummy, &precision);
+  channels = afGetChannels(affd, AF_DEFAULT_TRACK);
+  byteorder = afGetVirtualByteOrder(affd, AF_DEFAULT_TRACK);
+  framesize = (int) afGetFrameSize(affd, AF_DEFAULT_TRACK, 0);
+  framecount = afGetFrameCount(affd, AF_DEFAULT_TRACK);
+  sample->speed = (int) afGetRate(affd, AF_DEFAULT_TRACK);
+
+  if (channels <= 2)
+    sample->channels = channels;
+  else
+    goto error_out;
+
+  switch (precision)
+    {
+      case 8:
+	sample->format = SND_PCM_FORMAT_S8;
+	break;
+      case 16:
+	if (byteorder == AF_BYTEORDER_LITTLEENDIAN)
+	  sample->format = SND_PCM_FORMAT_S16_LE;
+	else
+	  sample->format = SND_PCM_FORMAT_S16_BE;
+	break;
+      default:
+	goto error_out;
+	break;
+    }
+
+  sample->framesize = framesize;
   sample->periods = sample->framesize;
   sample->buffersize = (sample->periods * 8192) >> 2;
+  sample->framecount = framecount;
+  sample->audiodatalen = framecount * framesize;
+
+  sample->audiodata = (char *) malloc(sample->audiodatalen);
+  if (sample->audiodata != NULL)
+    {
+      ret = afReadFrames(affd, AF_DEFAULT_TRACK, sample->audiodata, framecount);
+      if (ret != framecount)
+	{
+	  free(sample->audiodata);
+	  goto error_out;
+	}
+    }
+  else
+    goto error_out;
+
+  afCloseFile(affd);
 
   return sample;
+
+ error_out: /* something bad happened */
+  afCloseFile(affd);
+  free(sample);
+  return NULL;
 }
 
 
@@ -474,9 +477,6 @@ beep_thread_command(int command)
   if (!beep_thread_running)
     return;
 
-  if (!_dsp.sample)
-    return;
-
   pthread_mutex_lock(&(_dsp.mutex));
 
   _dsp.command = command;
@@ -491,9 +491,6 @@ static void
 beep_thread_cleanup(void)
 {
   int i;
-
-  if (!_dsp.sample)
-    return;
 
   for (i = 0; i < AUDIO_N; i++)
     {
