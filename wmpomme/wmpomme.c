c9cd7f6459b816c21c260f2cc983969334fa520f
Julien BLACHE <jb@jblache.org>
2008-11-09 15:54:56 +0000

    Output errors to stderr.

--- wmpomme/wmpomme.c
+++ wmpomme/wmpomme.c
@@ -209,7 +209,7 @@ mbp_dbus_listen(void)
 	}
       else if (dbus_message_is_signal(msg, DBUS_INTERFACE_LOCAL, "Disconnected"))
 	{
-	  printf("DBus disconnected\n");
+	  fprintf(stderr, "DBus disconnected\n");
 
 	  mbpdisplay = DISPLAY_FLAG_UPDATE | DISPLAY_TYPE_DBUS_NOK;
 
@@ -236,7 +236,7 @@ wmmbp_lcd_getlevel_cb(DBusPendingCall *pending, void *status)
 
   if (msg == NULL)
     {
-      printf("Could not steal reply\n");
+      fprintf(stderr, "Could not steal reply\n");
 
       dbus_pending_call_unref(pending);
 
@@ -267,7 +267,7 @@ wmmbp_kbd_getlevel_cb(DBusPendingCall *pending, void *status)
 
   if (msg == NULL)
     {
-      printf("Could not steal reply\n");
+      fprintf(stderr, "Could not steal reply\n");
 
       dbus_pending_call_unref(pending);
 
@@ -298,7 +298,7 @@ wmmbp_ambient_getlevel_cb(DBusPendingCall *pending, void *status)
 
   if (msg == NULL)
     {
-      printf("Could not steal reply\n");
+      fprintf(stderr, "Could not steal reply\n");
 
       dbus_pending_call_unref(pending);
 
@@ -330,7 +330,7 @@ wmmbp_audio_getvolume_cb(DBusPendingCall *pending, void *status)
 
   if (msg == NULL)
     {
-      printf("Could not steal reply\n");
+      fprintf(stderr, "Could not steal reply\n");
 
       dbus_pending_call_unref(pending);
 
@@ -361,7 +361,7 @@ wmmbp_audio_getmute_cb(DBusPendingCall *pending, void *status)
 
   if (msg == NULL)
     {
-      printf("Could not steal reply\n");
+      fprintf(stderr, "Could not steal reply\n");
 
       dbus_pending_call_unref(pending);
 
@@ -392,35 +392,35 @@ wmmbp_get_values(void)
   ret = mbp_call_lcd_getlevel(wmmbp_lcd_getlevel_cb, &cbret);
   if ((ret < 0) || (cbret < 0))
     {
-      printf("lcdBacklight getLevel call failed !\n");
+      fprintf(stderr, "lcdBacklight getLevel call failed !\n");
       goto mcall_error;
     }
 
   ret = mbp_call_kbd_getlevel(wmmbp_kbd_getlevel_cb, &cbret);
   if ((ret < 0) || (cbret < 0))
     {
-      printf("kbdBacklight getLevel call failed !\n");
+      fprintf(stderr, "kbdBacklight getLevel call failed !\n");
       goto mcall_error;
     }
 
   ret = mbp_call_ambient_getlevel(wmmbp_ambient_getlevel_cb, &cbret);
   if ((ret < 0) || (cbret < 0))
     {
-      printf("ambient getLevel call failed !\n");
+      fprintf(stderr, "ambient getLevel call failed !\n");
       goto mcall_error;
     }
 
   ret = mbp_call_audio_getvolume(wmmbp_audio_getvolume_cb, &cbret);
   if ((ret < 0) || (cbret < 0))
     {
-      printf("audio getVolume call failed !\n");
+      fprintf(stderr, "audio getVolume call failed !\n");
       goto mcall_error;
     }
 
   ret = mbp_call_audio_getmute(wmmbp_audio_getmute_cb, &cbret);
   if ((ret < 0) || (cbret < 0))
     {
-      printf("audio getMute call failed !\n");
+      fprintf(stderr, "audio getMute call failed !\n");
       goto mcall_error;
     }
 
