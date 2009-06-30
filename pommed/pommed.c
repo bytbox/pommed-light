cbe0ab7c1d8f8cce53347f201ae16f66ab5c5145
Julien BLACHE <jb@jblache.org>
2009-06-30 17:50:59 +0200

    Add support for the MacBookPro5,2 (17" MacBookPro June 2009)

--- pommed/pommed.c
+++ pommed/pommed.c
@@ -276,6 +276,7 @@ struct machine_ops mb_mops[] = {
   },
 
   {  /* MacBookPro5,1 (15" & 17", Core2 Duo, October 2008)
+      * MacBookPro5,2 (17" June 2009)
       * MacBookPro5,5 (13" June 2009) */
     .type = MACHINE_MACBOOKPRO_5,
     .lcd_backlight_probe = mbp_sysfs_backlight_probe,
@@ -672,8 +673,10 @@ check_machine_dmi(void)
   else if (strcmp(buf, "MacBookPro4,1") == 0)
     ret = MACHINE_MACBOOKPRO_4;
   /* Core2 Duo MacBook Pro 15" & 17" (October 2008)
+   * MacBook Pro 17" (June 2009)
    * MacBook Pro 13" (June 2009) */
   else if ((strcmp(buf, "MacBookPro5,1") == 0)
+	   || (strcmp(buf, "MacBookPro5,2") == 0)
 	   || (strcmp(buf, "MacBookPro5,5") == 0))
     ret = MACHINE_MACBOOKPRO_5;
   /* Core Duo MacBook (May 2006) */
