e0e4ef3b99f7056ca9f8c41796ee9bdb6eb72278
Julien BLACHE <jb@jblache.org>
2010-01-10 11:05:42 +0100

    Add the MacBookPro5,4 (15" June 2009)

--- pommed/pommed.c
+++ pommed/pommed.c
@@ -278,6 +278,7 @@ struct machine_ops mb_mops[] = {
   {  /* MacBookPro5,1 (15" & 17", Core2 Duo, October 2008)
       * MacBookPro5,2 (17" June 2009)
       * MacBookPro5,3 (15" June 2009)
+      * MacBookPro5,4 (15" June 2009)
       * MacBookPro5,5 (13" June 2009) */
     .type = MACHINE_MACBOOKPRO_5,
     .lcd_backlight_probe = mbp_sysfs_backlight_probe,
@@ -679,6 +680,7 @@ check_machine_dmi(void)
   else if ((strcmp(buf, "MacBookPro5,1") == 0)
 	   || (strcmp(buf, "MacBookPro5,2") == 0)
 	   || (strcmp(buf, "MacBookPro5,3") == 0)
+	   || (strcmp(buf, "MacBookPro5,4") == 0)
 	   || (strcmp(buf, "MacBookPro5,5") == 0))
     ret = MACHINE_MACBOOKPRO_5;
   /* Core Duo MacBook (May 2006) */
